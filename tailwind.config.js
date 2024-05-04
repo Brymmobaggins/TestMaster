da67db4ae09fff34b052da519d00ee820768b444
brymmobaggins <ibro2tablet21@yahoo.com>
2024-05-04 16:43:23 +0100

    fix homepage

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      playBall: ["'Playball'", "cursive"],
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
